61f7beacc105f52ce334cc5d7424ae864c30fcf2
Shema John <johnshema7@gmail.com>
2025-08-01 13:38:51 +0200

    My bro's Portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    },
+    base: '/shema-kevin---personal-portfolio/', // ✅ This MUST match your GitHub repo name
+    plugins: [ghPages()]
+  };
 });
